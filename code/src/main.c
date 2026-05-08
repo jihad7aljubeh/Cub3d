ebd72a7cd84eeec9ff1d678648bfe48d3194debd
Jihad7aljubeh <aljubehjihad@gmail.com>
2026-05-08 20:00:18 +0300

    1

--- code/src/main.c
+++ code/src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jalju-be <jalju-be@student.42amman.com>    +#+  +:+       +#+        */
+/*   By: jehad <jehad@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/22 16:03:22 by aabusnin          #+#    #+#             */
-/*   Updated: 2026/05/05 19:18:40 by jalju-be         ###   ########.fr       */
+/*   Updated: 2026/05/08 19:51:56 by jehad            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,9 @@ int	main(int argc, char **argv)
 	if (argc != 2)
 		error_exit("Usage: ./cub3D map.cub");
 	ft_memset(&game, 0, sizeof(t_game));
-	init_game2(&game);
+	init_game(&game);
 	if (!parse_map(&game, argv[1]))
 		error_exit("Parsing failed");
-	init_game2(&game);
 	load_textures(&game);
 	setup_hooks(&game);
 	mlx_loop_hook(game.mlx, render_frame, &game);
